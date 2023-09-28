9fcedb4427dcf13bcb14c9ee9f82084121ad2335
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-09-28 20:33:38 +0200

    add method in CarServiceTest:
    - testFindAllByCategory_WhenCategoryNotExists_ShouldReturnEmptyPage

--- car-microservice/src/test/java/ua/foxminded/car/microservice/service/CarServiceTest.java
+++ car-microservice/src/test/java/ua/foxminded/car/microservice/service/CarServiceTest.java
@@ -141,7 +141,15 @@ class CarServiceTest {
 
     @ParameterizedTest
     @CsvSource({ "Sedan, 0, 10, make, asc", "Hatchback, 0, 10, make, asc", "SUV, 0, 10, make, asc" })
-    void testFindAllByCategory(String categoryName, int page, int size, String sortBy, String sortOrder) {
+    void testFindAllByCategory_ShouldReturnCarsAssignedToCategory(String categoryName, int page, int size,
+            String sortBy, String sortOrder) {
         Assertions.assertTrue(!carService.findCarsByCategory(categoryName, page, size, sortBy, sortOrder).isEmpty());
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "testCategory, 0, 10, make, asc" })
+    void testFindAllByCategory_WhenCategoryNotExists_ShouldReturnEmptyPage(String categoryName, int page, int size,
+            String sortBy, String sortOrder) {
+        Assertions.assertTrue(carService.findCarsByCategory(categoryName, page, size, sortBy, sortOrder).isEmpty());
     }
 }
