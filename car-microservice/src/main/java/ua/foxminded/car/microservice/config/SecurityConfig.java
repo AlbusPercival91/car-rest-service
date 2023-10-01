18296d493621b6ddecf3981d53f8938599bff157
Sergiy Tarasevych <sergeytarasevich2012@gmail.com>
2023-10-01 15:30:05 +0200

    adjust securityConfig
    refactor pom.xml

--- car-microservice/src/main/java/ua/foxminded/car/microservice/config/SecurityConfig.java
+++ car-microservice/src/main/java/ua/foxminded/car/microservice/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package ua.foxminded.car.microservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,11 +16,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(auth -> {
-            auth.requestMatchers("/").permitAll();
-            auth.anyRequest().authenticated();
-        })
-
-                .oauth2Login(withDefaults()).formLogin(Customizer.withDefaults()).build();
-
+            auth.requestMatchers(HttpMethod.GET, "/api/v1/cars/**", "/api/v1/categories/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/v1/cars/**", "/api/v1/categories/**").authenticated()
+                    .requestMatchers(HttpMethod.PUT, "/api/v1/cars/**", "/api/v1/categories/**").authenticated()
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/cars/**", "/api/v1/categories/**").authenticated()
+                    .anyRequest().authenticated();
+        }).oauth2Login(withDefaults()).formLogin(Customizer.withDefaults()).build();
     }
 }
